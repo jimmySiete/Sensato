ab25b774c5e5a39cd5db0f6871e1a607fd0030cc
LinaMaSe <karo_14sp@hotmail.com>
2021-02-12 11:22:36 -0600

    segundo intento

--- Sensato.Test/Program.cs
+++ Sensato.Test/Program.cs
@@ -12,31 +12,33 @@ namespace SENSATO.Test
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            XmlDocument doc = new XmlDocument();
-            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-            XmlElement root = doc.DocumentElement;
-            doc.InsertBefore(xmlDeclaration, root);
+        
 
-            XmlElement element1 = doc.CreateElement(string.Empty, "cuerpo", string.Empty);
-            doc.AppendChild(element1);
-            XmlElement element2 = doc.CreateElement(string.Empty, "nivel1", string.Empty);
-            element1.AppendChild(element2);
-            XmlElement element3 = doc.CreateElement(string.Empty, "nivel2", string.Empty);
-            element2.AppendChild(element3);
+        //static void Main(string[] args)
+        //{
+        //    XmlDocument doc = new XmlDocument();
+        //    XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+        //    XmlElement root = doc.DocumentElement;
+        //    doc.InsertBefore(xmlDeclaration, root);
 
-            XmlText text1 = doc.CreateTextNode("texto");
-            element3.AppendChild(text1);
-            element2.AppendChild(element3);
+        //    XmlElement element1 = doc.CreateElement(string.Empty, "cuerpo", string.Empty);
+        //    doc.AppendChild(element1);
+        //    XmlElement element2 = doc.CreateElement(string.Empty, "nivel1", string.Empty);
+        //    element1.AppendChild(element2);
+        //    XmlElement element3 = doc.CreateElement(string.Empty, "nivel2", string.Empty);
+        //    element2.AppendChild(element3);
 
-            XmlElement element4 = doc.CreateElement(string.Empty, "nivel3", string.Empty);
-            XmlText text2 = doc.CreateTextNode("más texto");
-            element4.AppendChild(text2);
-            element2.AppendChild(element4);
+        //    XmlText text1 = doc.CreateTextNode("texto");
+        //    element3.AppendChild(text1);
+        //    element2.AppendChild(element3);
 
-            doc.Save("C:/Users/Carolina Martinez/Desktop/XMLSample.xml");
-        }
+        //    XmlElement element4 = doc.CreateElement(string.Empty, "nivel3", string.Empty);
+        //    XmlText text2 = doc.CreateTextNode("más texto");
+        //    element4.AppendChild(text2);
+        //    element2.AppendChild(element4);
+
+        //    doc.Save("C:/Users/Carolina Martinez/Desktop/XMLSample.xml");
+        //}
 
 
 
