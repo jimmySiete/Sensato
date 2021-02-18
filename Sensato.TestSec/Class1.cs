1056f234f4611210cf2be63c436de6d04e0883f1
Castañon, Jaime Fabian <jcastanonescobedo@sensata.com>
2021-02-18 15:51:10 -0600

    Ejemplos

---
+++ Sensato.TestSec/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensato.TestSec
+{
+    public class Class1
+    {
+
+
+    }
+}
---
+++ Sensato.TestSec/Repository/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensato.TestSec
+{
+    public partial class UserContext
+    {
+    }
+}
